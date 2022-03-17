0c159c18cb2ef75ce7186c0824fb3cd830fec8d3
eyacquah <eyacquah0@gmail.com>
2022-03-17 16:23:40 +0000

    Updated socket url

--- src/util/socket.js
+++ src/util/socket.js
@@ -1,4 +1,4 @@
 export const SOCKET_URL =
   process.env.NODE_ENV === "development"
     ? "ws://localhost:8080/ws"
-    : "ws://https://go-pong-server.uc.r.appspot.com/ws";
+    : "wss://https://go-pong-server.uc.r.appspot.com/ws";
