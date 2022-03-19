0a31e75385379d2166083142d701ad02d2dd4829
eyacquah <eyacquah0@gmail.com>
2022-03-19 14:25:48 +0000

    Changes to start screen

--- src/components/screens/StartGame.js
+++ src/components/screens/StartGame.js
@@ -4,23 +4,15 @@ const StartGame = ({ changeScreen }) => {
   const handleClick = () => changeScreen("play");
 
   return (
-    <div style={{ textAlign: "center", margin: "30px", padding: "50px" }}>
+    <div style={{ textAlign: "center", margin: "100px", padding: "50px" }}>
       <h1>Welcome to React Pong 😀😀</h1>
-      <p>
-        This Game's frontend was built with ReactJS. It communicates with a
-        Golang backend via websockets. You're welcome to try it out and send
-        your feedback
-      </p>
+      <h2>React😍 + Go💪 via websockets⚡</h2>
       <h5>Game Play</h5>
-      <div>
-        <p>
-          The game consists of two players. Player 1 uses the W & S keys to move
-          up and down. Player 2 uses the Up & Down Arrow keys to move up and
-          down. A player's score is incremented by 1 each time his/her pad
-          connects with the ball. The first player to reach a target score of 50
-          wins the game.
-        </p>
-      </div>
+
+      <p>
+        Player 1 uses W & S keys to move up and down. Player 2 uses the Up &
+        Down Arrow keys to move up and down. First to 10 wins.
+      </p>
 
       <Button text="Start Game" onClick={handleClick} />
     </div>
